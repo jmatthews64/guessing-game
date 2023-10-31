8dd2369dd42f23e231845c0ce3b612c8bafae8a4
jmatthews64 <jon.matthews64@gmail.com>
2023-10-31 13:59:16 -0400

    Made notes for each part for future programers

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import random
 
-
+#asks the user if they want to play and initializes game if yes
 play_time = input("Are you ready to play (Y or N): ")
 play_time = play_time.upper()
 game_on = None
 
+#function to run the guessing 
 def guessing_game():
+
+    #initialize a random number as the answer
     answer = random.randint(1, 20)
-    print(answer)
+    
+    #Creats a while loop giving the player 4 guesses and upon a correct guess or runs out of guesses ends the game
     count = 4
     while count > 0:
         print("You have ", count, "guesses remaining.")
@@ -22,6 +26,7 @@ def guessing_game():
         count -= 1
     print("Game over.  The correct answer was: ", answer)
 
+#creates a function to allow the user to keep playing or quit after playing 1 or more times
 def playing ():
     keep_playing = input("Do you want to play again (Y or N)? ")
     keep_playing = keep_playing.upper()
@@ -29,14 +34,17 @@ def playing ():
         print("Goodbye")
         quit()    
 
+#initializes the game for the first run
 if play_time == "Y":
     game_on = True
 else:
     game_on = False
-    
+
+#runs the game by calling the game function and keeps game going by calling the playing function    
 while game_on != False:
     guessing_game()
     playing()
 
+#If the player doesn't want to keep playing, this quits the game
 print("Goodbye!")
 quit()
